a5f541dd1d68e70f15575a2a5d555a500c1be3ac
47-r0nan <ronan.harris1@gmail.com>
2023-05-17 16:38:38 +0100

    Folder Initialisiation

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,7 @@
+// PascalCasing - used in react for functions, like camel casing
+function Message() {
+    // JSX: JavaScript XML (Syntax, babeljs.io for translater)
+    return <h1>Hello World!</h1>;
+}
+
+export default Message;
