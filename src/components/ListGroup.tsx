dcb37ea63a7795387d23d18eaffe71ca529dcedc
Ivan Lapickij <ivan.lapickij@gmail.com>
2023-05-21 17:40:36 +0100

    Managing States

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,17 @@
-import { MouseEvent } from "react";
+// import { MouseEvent } from "react";
+
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   // const message = items.length === 0 ? <p>no item found</p> : null
 
   // const getMessage = () => { return items.length === 0 ? <p>no item found</p> : null}
   // Event Handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
@@ -15,10 +19,18 @@ function ListGroup() {
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className="list-group-item"
+            // className="list-group-item active"
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
             // onClick={() => console.log(item, index)}
-            onClick={handleClick}
+            // onClick={handleClick}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
           >
             {item}
           </li>
