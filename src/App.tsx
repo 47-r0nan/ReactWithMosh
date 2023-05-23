db08dcac076915da94a9696a347269ec75962397
47-r0nan <ronan.harris1@gmail.com>
2023-05-23 12:12:05 +0100

    Like Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import ButtonExercise from "./components/ButtonExercise";
-import DismissableAlert from "./components/DismissableAlert";
-import { useState } from "react";
+import Like from "./components/Like";
 
-function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+const App = () => {
   return (
     <div>
-      {alertVisible && (
-        <DismissableAlert onClose={() => setAlertVisibility(false)} />
-      )}
-      <ButtonExercise onClick={() => setAlertVisibility(true)}>
-        My Button
-      </ButtonExercise>
+      <Like onClick={() => console.log("clicked")} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { useState } from "react";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 
-const Like = () => {
-  return <div></div>;
+interface LikeProps {
+  onClick: () => void;
+}
+
+const Like = ({ onClick }: LikeProps) => {
+  const [status, setStatus] = useState(false);
+
+  const toggle = () => {
+    setStatus(!status);
+    onClick();
+  };
+
+  if (status) return <AiFillHeart color="red" size={20} onClick={toggle} />;
+  return <AiOutlineHeart size={20} onClick={toggle} />;
 };
 
 export default Like;
