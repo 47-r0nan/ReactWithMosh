215fb969f3ad9a2eb25c6aa1a695fa5a9178fcfc
Ivan Lapickij <ivan.lapickij@gmail.com>
2023-05-23 15:44:36 +0100

    Revert "Handling events"
    This reverts commit 0f5379f3cad8209b42968484eeb773252f7d976e.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,43 +1,24 @@
-// import { MouseEvent } from "react";
+import { MouseEvent } from "react";
 
-import { useState } from "react";
-//{items : [], heading: string}
-interface Props {
-  items: string[];
-  heading: string;
-  //(item: string) => void
-  onSelectItem: (item: string) => void;
-}
-
-function ListGroup({ items, heading, onSelectItem }: Props) {
-  // Hook
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
   // const message = items.length === 0 ? <p>no item found</p> : null
 
   // const getMessage = () => { return items.length === 0 ? <p>no item found</p> : null}
   // Event Handler
-  // const handleClick = (event: MouseEvent) => console.log(event);
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
-      <h1>{heading}</h1>
+      <h1>List</h1>
       {items.length === 0 && <p>no item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            // className="list-group-item active"
-            className={
-              selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
-            }
+            className="list-group-item"
             key={item}
             // onClick={() => console.log(item, index)}
-            // onClick={handleClick}
-            onClick={() => {
-              setSelectedIndex(index);
-              onSelectItem(item);
-            }}
+            onClick={handleClick}
           >
             {item}
           </li>
@@ -46,5 +27,4 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
     </>
   );
 }
-
 export default ListGroup;
