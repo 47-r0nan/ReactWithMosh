6c5deceef0e5c6e153d1fbb01e38e254804484b6
47-r0nan <ronan.harris1@gmail.com>
2023-05-23 10:08:16 +0100

    Finished YT Tutorial and completed alert exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import ButtonExercise from "./components/ButtonExercise";
+import DismissableAlert from "./components/DismissableAlert";
+import { useState } from "react";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <ButtonExercise onClick={() => console.log("Clicked")}>
+      {alertVisible && (
+        <DismissableAlert onClose={() => setAlertVisibility(false)} />
+      )}
+      <ButtonExercise onClick={() => setAlertVisibility(true)}>
         My Button
       </ButtonExercise>
     </div>
---
+++ src/components/DismissableAlert.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react";
+
+interface DismissableAlertProps {
+  onClose: () => void;
+}
+
+const DismissableAlert = ({ onClose }: DismissableAlertProps) => {
+  return (
+    <div
+      className="alert alert-warning alert-dismissible fade show"
+      role="alert"
+    >
+      <strong>Holy guacamole!</strong> You should check in on some of those
+      fields below.
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
+};
+
+export default DismissableAlert;
